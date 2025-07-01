5db7262a95561807796994d770007ee2be234ff6
Reboya237 <madecccons@gmail.com>
2025-07-02 00:38:25 +0200

    Update momo-request-live.js

--- netlify/functions/momo-request-live.js
+++ netlify/functions/momo-request-live.js
@@ -4,7 +4,7 @@ const fetch = require("node-fetch");
 exports.handler = async function (event) {
   try {
     const { amount, phone } = JSON.parse(event.body);
-    const subscriptionKey = "YOUR_LIVE_PRIMARY_KEY"; // Replace this with your live key
+    const subscriptionKey = "3c9aa91fc6ca42a8989180f12d037a79"; // Replace this with your live key
 
     const tokenRes = await fetch("https://momodeveloper.mtn.com/collection/token/", {
       method: "POST",
